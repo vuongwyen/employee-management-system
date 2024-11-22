ec40d142e57c69d08dfe5f6485de2363794a8d0b
vuongwyen <treepoo2023@gmail.com>
2024-11-22 22:15:56 +0700

    Update code

--- employeeLogin.php
+++ employeeLogin.php
@@ -1,41 +1,63 @@
 <?php
-include "connection/connect.php";
 
+@include 'connection/connect.php';
+
+session_start();
+
+if (isset($_POST['submit'])) {
+
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $pass = $_POST['password'];
+
+    $select = "SELECT * FROM employee WHERE emp_email = '$email' AND emp_pass = '$pass'";
+
+    $result = mysqli_query($con, $select);
+
+    if (mysqli_num_rows($result) > 0) {
+
+    header('location:employee/index.php');
+    exit;
+
+    } else {
+        $error[] = 'Email hoặc mật khẩu không đúng!';
+    }
+}
+if (isset($error)) {
+    foreach ($error as $err) {
+        echo '<p style="color:red;">' . $err . '</p>';
+    }
+}
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   <title>Login form</title>
+
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 offset-md-3">
-                <h2 class="text-center text-dark mt-5">Admin login</h2>   
-                <div class="card my-5">
-                    <form class="card-body cardbody-color p-lg-5">
-                        <div class="text-center">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAExe-8gw8f6a9EiBUzjmsavX03D_spiSyaw&s" class="img-fluid profile-image-pic img-thumbnail rounded-circle my-3"width="200px" alt="profile">
-                        </div>
-                        <div class="mb-3">
-                            <p>Email *</p>
-                            <input type="text" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Please enter your email">
-                        </div>
-                        <div class="mb-3">
-                            <p>Password *</p>
-                            <input type="password" name="password" class="form-control" id="password" placeholder="Please ennter your password">
-                        </div>
-                        <div class="text-center"><button type="submit" class="btn btn-primary">Login</button></div>
-                        <p>You are admin? <a href="index.php">Login here.</a></p>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>  
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+   
+<div class="form position-absolute top-50 start-50 translate-middle">
+
+   <form action="" method="post">
+      <h1>Employee login</h1>
+      <br>
+      <div class="form-outline mb-4">
+        <input type="email" name="email" required placeholder="Enter your email" class="form-control">
+      </div>
+      <div class="form-outline mb-4">
+        <input type="password" name="password" required placeholder="enter your password" class="form-control">
+      </div>
+      
+      <input type="submit" name="submit" value="login now" class="btn btn-primary btn-block mb-4">
+      <p>You are admin? <a href="index.php">Login here</a></p>
+   </form>
+
+</div>
+
 </body>
 </html>
