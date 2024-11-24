ba4ecfb5d555bfb14f1caa7155dfc58d4fa009c6
vuongwyen <treepoo2023@gmail.com>
2024-11-24 14:10:55 +0700

    update delete of job department

--- admin/operation/jobDepartment/delete.php
+++ admin/operation/jobDepartment/delete.php
@@ -4,8 +4,8 @@ if(!$con){
     die(mysqli_errno($con));
 }
 if(isset($_GET['deletejob_ID'])){
-    $emp_ID=$_GET['deletejob_ID'];
-    $sql="DELETE FROM job_department WHERE deletejob_ID='$job_ID'";
+    $job_ID=$_GET['deletejob_ID'];
+    $sql="DELETE FROM job_department WHERE job_ID='$job_ID'";
     $result=mysqli_query($con,$sql);
     if($result){
         header("Location:../../../admin/jobDepartment.php");
